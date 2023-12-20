a9ffdf4b884801de167e68997876a32eefba42cc
AstroDev <ferrari.codefire@hotmail.com>
2023-12-20 13:09:30 -0500

    fix MIME Integration

--- server.js
+++ server.js
@@ -5,31 +5,29 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Define MIME type for CSS files
-express.static.mime.define({ 'text/css': ['css'] });
+app.use('/static', express.static(path.join(__dirname, 'static')));
 
-// Serve static files
-app.use(express.static(path.join(__dirname, 'static')));
-
-// Body parser middleware for parsing form data
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Define routes
+app.get('/static/css/main.css', (req, res, next) => {
+  res.header('Content-Type', 'text/css');
+  next();
+});
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/index.html'));
+  res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
 app.post('/submit-form', (req, res) => {
-    const inputData = req.body.data;
+  const inputData = req.body.data;
 
-    /*
-     * Add API integrations here later.
-     * For now, all this is doing is echoing data transfer in the form.
-     */
-    res.send(`Received data: ${inputData}`);
+  /*
+   * Add API integrations here later.
+   * For now, all this is doing is echoing data transfer in the form.
+   */
+  res.send(`Received data: ${inputData}`);
 });
 
-// Start the server
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
