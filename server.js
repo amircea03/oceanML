54f38f613107c07aa993f4dee41de410a1fc05f1
AstroDev <ferrari.codefire@hotmail.com>
2023-12-20 12:09:00 -0500

    Form submission:

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
-// Template for server.
-
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'static')));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+
+app.post('/submit-form', (req, res) => {
+    const inputData = req.body.data;
+
+    /*
+     * Add API integrations here later.
+     * For now all this is doing is echoing data transfer in the form.
+     */
+    res.send(`Received data: ${inputData}`);
 });
 
 app.listen(port, () => {
