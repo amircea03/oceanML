02c17407eb6541c071ef22d61aff046531034e51
AstroDev <ferrari.codefire@hotmail.com>
2023-12-20 12:54:55 -0500

    fix MIME Integration

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Define MIME type for CSS files
+express.static.mime.define({ 'text/css': ['css'] });
+
+// Serve static files
 app.use(express.static(path.join(__dirname, 'static')));
+
+// Body parser middleware for parsing form data
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Define routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'));
 });
@@ -16,11 +24,12 @@ app.post('/submit-form', (req, res) => {
 
     /*
      * Add API integrations here later.
-     * For now all this is doing is echoing data transfer in the form.
+     * For now, all this is doing is echoing data transfer in the form.
      */
     res.send(`Received data: ${inputData}`);
 });
 
+// Start the server
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
