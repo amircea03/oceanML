40a796ca1f0004cc29a79150510ff641e12fff22
AstroDev <ferrari.codefire@hotmail.com>
2023-12-20 15:22:44 -0500

    React link to Express

--- oceanml-fe/src/App.js
+++ oceanml-fe/src/App.js
@@ -1,6 +1,5 @@
-// oceanml-frontend/src/App.js
-
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import './App.css';
 
 function App() {
   const [data, setData] = useState('');
