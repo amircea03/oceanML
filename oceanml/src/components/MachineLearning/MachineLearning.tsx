239f878a01b551a3fc6c6b92ec1fdeaf3149b184
AstroDev <ferrari.codefire@hotmail.com>
2023-12-21 16:39:11 -0500

    ML page finished

--- oceanml/src/components/MachineLearning/MachineLearning.tsx
+++ oceanml/src/components/MachineLearning/MachineLearning.tsx
@@ -6,11 +6,15 @@ const MachineLearning: React.FC = () => {
     return(
         <div className="dashboard-container">
         <div className="top-bar">
-          <h2>About OceanML</h2>
+          <h2>Machine Learning</h2>
         </div>
         <Link to="/" className="box box1">
           Dashboard
         </Link>
+        <div className="text-container">
+          <h1>OceanML "Machine Learning"</h1>
+          <p>OceanML's Machine Learning Platform</p>
+        </div>
         <div className="footer">
           <p>&copy; 2023 OceanML. All rights reserved.</p>
         </div>
