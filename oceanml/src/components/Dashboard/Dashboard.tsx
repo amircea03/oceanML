67171441ccaa94fffe97e845b61d92c799ac7afd
AstroDev <ferrari.codefire@hotmail.com>
2023-12-21 14:25:53 -0500

    footer style

--- oceanml/src/components/Dashboard/Dashboard.tsx
+++ oceanml/src/components/Dashboard/Dashboard.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Dashboard.scss';
 
-
 const Dashboard: React.FC = () => {
   return (
     <div className="dashboard-container">
@@ -17,6 +16,9 @@ const Dashboard: React.FC = () => {
       <div className="box box4">Box 4</div>
       <div className="box box5">Box 5</div>
       <div className="box box6">Box 6</div>
+      <div className="footer">
+        <p>&copy; 2023 OceanML. All rights reserved.</p>
+      </div>
     </div>
   );
 };
