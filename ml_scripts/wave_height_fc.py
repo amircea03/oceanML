db53b5677c887979c087ed51aa4fbaa9ccb43de7
Andrei Mircea <amircea@umich.edu>
2023-12-21 17:33:15 -0500

    Adding wave height file

---
+++ ml_scripts/wave_height_fc.py
@@ -0,0 +1,40 @@
+# Wave Height Model
+
+import openmeteo_requests
+
+import requests_cache
+import pandas as pd
+from retry_requests import retry
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+url = "https://marine-api.open-meteo.com/v1/marine"
+params = {
+	"latitude": 54.3213,
+	"longitude": 10.1348,
+	"hourly": "wave_height"
+}
+responses = openmeteo.weather_api(url, params=params)
+response = responses[0]
+print(f"Coordinates {response.Latitude()}°E {response.Longitude()}°N")
+print(f"Elevation {response.Elevation()} m asl")
+print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+# Process hourly data. The order of variables needs to be the same as requested.
+hourly = response.Hourly()
+hourly_wave_height = hourly.Variables(0).ValuesAsNumpy()
+
+hourly_data = {"date": pd.date_range(
+	start = pd.to_datetime(hourly.Time(), unit = "s"),
+	end = pd.to_datetime(hourly.TimeEnd(), unit = "s"),
+	freq = pd.Timedelta(seconds = hourly.Interval()),
+	inclusive = "left"
+)}
+hourly_data["wave_height"] = hourly_wave_height
+
+hourly_dataframe = pd.DataFrame(data = hourly_data)
+print(hourly_dataframe)
