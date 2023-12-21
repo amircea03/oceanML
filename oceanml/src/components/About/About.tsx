413b1ea1732dc6571232c6054de92aa42ea5af5a
AstroDev <ferrari.codefire@hotmail.com>
2023-12-21 14:03:11 -0500

    About page

---
+++ oceanml/src/components/About/About.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './About.scss';
+
+const About: React.FC = () => {
+  return (
+    <div className="about-container">
+      <h1>About Page</h1>
+      {/* Add your content and styling for the About page here */}
+    </div>
+  );
+};
+
+export default About;
