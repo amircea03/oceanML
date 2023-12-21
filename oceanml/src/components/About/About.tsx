0737a13f48f59266c03fdadb8b681f8682db159b
AstroDev <ferrari.codefire@hotmail.com>
2023-12-21 15:05:14 -0500

    Linking dashboard to about #1

--- oceanml/src/components/About/About.tsx
+++ oceanml/src/components/About/About.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import './About.scss';
+import { Link } from 'react-router-dom';
+import '../Dashboard/Dashboard.scss';
 
 const About: React.FC = () => {
   return (
-    <div className="about-container">
-      <h1>About Page</h1>
-      {/* Add your content and styling for the About page here */}
+    <div className="dashboard-container">
+      <div className="top-bar">
+        <h2>About OceanML</h2>
+      </div>
+      <Link to="/" className="box box1">
+        Dashboard
+      </Link>
+      <div className="box box2">Box 2</div>
+      <div className="box box3">Box 3</div>
+      <div className="box box4">Box 4</div>
+      <div className="box box5">Box 5</div>
+      <div className="box box6">Box 6</div>
+      <div className="footer">
+        <p>&copy; 2023 OceanML. All rights reserved.</p>
+      </div>
     </div>
   );
 };
